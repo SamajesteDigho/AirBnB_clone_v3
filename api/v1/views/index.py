69d6158663add08643ee000c3af7e3a7bc90beee
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-10 11:13:42 +0800

    Exo 4, correction 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,28 @@
     Here we talk of the indexes
 """
 from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
 
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 @app_views.route('/status')
 def status():
     """ Return the page status """
     return {"status": "OK"}
+
+
+@app_views.route("/stats")
+def statistics():
+    """ Return a json of the statistics """
+    data = {}
+    for key, cls in classes.items():
+        data[key] = storage.count(cls=cls)
+    return data
