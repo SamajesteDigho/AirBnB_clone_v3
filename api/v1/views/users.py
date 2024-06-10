5aebf70ee47ef01d01de85e01447603db190ce13
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-11 06:30:15 +0800

    Exo 9

---
+++ api/v1/views/users.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+    Here we manage all the User urls
+"""
+from flask import jsonify, abort, request, make_response
+from models.user import User
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route("/users", methods=['GET'])
+def all_users():
+    """ Get all the Users """
+    users = storage.all(cls=User)
+    result = []
+    for _, x in users.items():
+        result.append(x.to_dict())
+    response = make_response(result, 200)
+    return response
+
+
+@app_views.route("/users/<user_id>", methods=['GET'])
+def user_by_id(user_id):
+    """ Get a user be id """
+    user = storage.get(cls=User, id=user_id)
+    if user is None:
+        abort(404)
+    response = make_response(user.to_dict(), 200)
+    return response
+
+
+@app_views.route("/users/<user_id>", methods=['DELETE'])
+def delete_user_by_id(user_id):
+    """ Delete a particular User """
+    user = storage.get(cls=User, id=user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    response = make_response({}, 200)
+    return response
+
+
+@app_views.route("/users", methods=['POST'])
+def new_user_object():
+    """ Creating new object """
+    try:
+        body = request.get_json()
+    except Exception:
+        abort(jsonify(error="Not a JSON"), 400)
+    if 'email' not in list(body.keys()):
+        abort(jsonify(error="Missing email"), 400)
+    if 'password' not in list(body.keys()):
+        abort(jsonify(error="Missing password"), 400)
+
+    user = User(email=body['email'], password=body['password'])
+    user.first_name = body.get('first_name', None)
+    user.last_name = body.get('last_name', None)
+    user.save()
+    response = make_response(user.to_dict(), 201)
+    return response
+
+
+@app_views.route("/users/<user_id>", methods=['PUT'])
+def update_user_by_id(user_id):
+    """ Lets update that user """
+    user = storage.get(cls=User, id=user_id)
+    if user is None:
+        abort(404)
+    try:
+        body = request.get_json()
+    except Exception:
+        abort(jsonify(error="Not a JSON"), 400)
+
+    user.first_name = body.get('first_name', user.first_name)
+    user.last_name = body.get('last_name', user.last_name)
+    user.password = body.get('password', user.password)
+    user.save()
+    response = make_response(user.to_dict(), 200)
+    return response
